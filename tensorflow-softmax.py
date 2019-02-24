2458b9d8af11cbb20f6bc8f94576068726062d33
Sérgio Mafra <sergio@mafra.io>
2019-02-23 21:20:30 -0300

    Added softmax

---
+++ tensorflow-softmax.py
@@ -0,0 +1,18 @@
+# Solution is available in the other "solution.ipynb"
+import tensorflow as tf
+
+
+def run():
+    output = None
+    logit_data = [2.0, 1.0, 0.1]
+    logits = tf.placeholder(tf.float32)
+
+    # TODO: Calculate the softmax of the logits
+    softmax = tf.nn.softmax(logit_data)
+
+    with tf.Session() as sess:
+        pass
+        # TODO: Feed in the logit data
+        output = sess.run(softmax, feed_dict={logits: logit_data})
+
+    return output
